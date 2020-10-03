08499dd6f905b473c5d64826f127de07b4141cd9
PromiSe#### <vic@iMac-PromiSe.local>
2020-10-03 11:33:06 +0300

    Поправил стэйты и вывод

--- components/Test1.tsx
+++ components/Test1.tsx
@@ -8,6 +8,7 @@ import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
 import { bin2dec, BinFormats } from "../lib/Bin2Dec";
 import { firstUpper } from "../lib/FirstUpper";
+import { createShareStore } from "../lib/SharedStore";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -32,28 +33,35 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+export const test1State = createShareStore({
+  input: '0',
+  format: BinFormats.INT8
+}, 'test1-store')
+
 export const Test1Component = () => {
   const classes = useStyles();
-  const [input, setInput] = useState('0')
-  const [format, setFormat] = useState(BinFormats.INT8)
+  const [state, setState] = test1State.useState()
+  const { input, format } = state
+
   const formats = Object.keys(BinFormats)
     .map(e => +e).filter(e => !isNaN(e))
-    .map(e => ({id: e, name: BinFormats[e]}))
+    .map(e => ({ id: e, name: BinFormats[e] }))
 
   const onChangeInput = ({ target: { value = '' } }) => {
     let length = +BinFormats[format].replace(/[^\d]+/g, '')
     value = value.replace(/[^01]+/g, '') || '0'
-    setInput(value.substr(-length))
+    setState({ ...state, input: value.substr(-length) })
   }
 
-  const onChangeFormat = ({target: {value = BinFormats.INT8}}) => {
-    setFormat(+value)
+  const onChangeFormat = ({ target: { value = BinFormats.INT8 } }) => {
+    setState({ ...state, format: +value })
   }
 
   return (
     <Grid container className={classes.root} spacing={2}>
-      <Grid item xs={4}>
+      <Grid item xs={6}>
         <TextField
+          style={{minWidth: 300}}
           onChange={onChangeInput}
           className={classes.rightAlign + ' ' + classes.formControl}
           label="Input Data" value={input} />
@@ -62,7 +70,7 @@ export const Test1Component = () => {
         <FormControl className={classes.formControl}>
           <InputLabel htmlFor="grouped-native-select">Format</InputLabel>
           <Select onChange={onChangeFormat as any} value={format} native>
-            {formats.map(({id, name}) => {
+            {formats.map(({ id, name }) => {
               name = firstUpper(name.replace(/(\d+)/, ' $1'))
               return (
                 <option key={id} value={id}>{name}</option>
@@ -71,9 +79,12 @@ export const Test1Component = () => {
           </Select>
         </FormControl>
         <hr />
-        <p>Output: <b>{bin2dec(input, format)}</b></p>
-      </Grid>
-      <Grid item xs={8}>
+        <h3>
+          Output: 
+          <b style={{color: 'red'}}>
+            {' ' + bin2dec(input, format)}
+          </b>
+        </h3>
       </Grid>
     </Grid>
   )
